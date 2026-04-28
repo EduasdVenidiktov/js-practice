ce4868f0352bb034697e4c3fc2a3c8fee8b86a0b
Eduard Venidiktov <edven7777777@gmail.com>
2026-04-29 00:37:04 +0300

    clear comments

--- js/task-6.js
+++ js/task-6.js
@@ -7,14 +7,6 @@ if ("serviceWorker" in navigator) {
   });
 }
 
-// self.addEventListener("install", () => {
-//   self.skipWaiting();
-// });
-
-// self.addEventListener("activate", () => {
-//   self.clients.claim();
-// });
-
 // 1. ФУНКЦІЯ ГОДИННИКА (працює автономно)
 function updateClock() {
   const timeElement = document.getElementById("current-time");
@@ -34,8 +26,6 @@ function getFormattedOldDate() {
   const day = String(date.getDate()).padStart(2, "0");
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const year = date.getFullYear();
-  //   const hours = String(date.getHours()).padStart(2, "0");
-  //   const minutes = String(date.getMinutes()).padStart(2, "0");
 
   return `Документ оновлено о 21:37 | ${day}.${month}.${year}`;
 }
@@ -52,16 +42,6 @@ if (runningLine) {
 
   runningLine.textContent = longText + longText; // дві копії → анімація на -50%
 }
-
-// const runningLine = document.getElementById("running-line");
-
-// if (runningLine) {
-//   const infoText = getFormattedOldDate();
-//   // Дублюємо текст для ефекту нескінченності
-//   // Повторюємо текст 5-6 разів, щоб заповнити всю ширину екрана
-//   const content = `${infoText} • `;
-//   runningLine.textContent = content.repeat(60);
-// }
 
 // Запускаємо годинник
 updateClock();
@@ -94,8 +74,6 @@ function installPWA() {
 document.getElementById("installBtn")?.addEventListener("click", installPWA);
 
 // 4. ЛОГІКА МОДАЛЬНОГО ВІКНА (Bottom Sheet)
-
-// ... (твій попередній код залишається без змін) ...
 
 document.addEventListener("DOMContentLoaded", () => {
   const openBtn = document.getElementById("open-modal-btn");
